63fa755711ac1df9b90732c554ce3db6ee394e42
Földi Zoltán Nándor <lagothzanta777@gmail.com>
2025-01-06 10:54:06 +0100

    hunglish(fix): workflow file 6

--- .test/changelog.sh
+++ .test/changelog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.name 'Github Actions Bot'
+git config --global user.email 'lagothzanta777@users.noreply.github.com'
+
 git log -n 5 --pretty=format:"%h - %s - %ad" --date=short > changelog-hunglish.txt
 git add changelog-hunglish.txt
 git commit --amend --no-edit
