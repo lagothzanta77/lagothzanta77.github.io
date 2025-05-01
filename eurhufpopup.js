614f1c7ff92273abf59fdd4e070ad468c0238f5c
Földi Zoltán Nándor <lagothzanta777@gmail.com>
2025-05-01 08:32:07 +0200

    fix(eurhuf): conversion

--- eurhufpopup.js
+++ eurhufpopup.js
@@ -8,7 +8,7 @@ document.getElementById('myid').addEventListener('input', function() {
     var sumValue = document.getElementById('myid').value;
     sumValue = sumValue.replace(/,/g, '.');
     if (!isNaN(sumValue) && sumValue !== '') {
-        document.getElementById('result').innerText = 'Forintban: ' + Math.round(parseFloat(sumValue * convertValue ));
+        document.getElementById('result').innerText = 'Ft: ' + Math.ceil(parseFloat(sumValue * convertValue ));
     } else {
         document.getElementById('result').innerText = 'Csak számot írj be!';
     }
@@ -22,13 +22,13 @@ document.getElementById('myeurid').addEventListener('input', function() {
 
     if ( !isNaN(sumValue) && sumValue !== '' && ( eurValue % 5 === 0 ) && ( eurValue >=0 ) )  {
         if ( sumValue * convertValue >= eurValue * convertValue ) {  
-            document.getElementById('result2').innerText = 'Forintban fizetendő még: ' + ( Math.round(parseFloat(sumValue * convertValue)) - Math.round(parseFloat(eurValue * convertValue)));
+            document.getElementById('result2').innerText = 'Ft még: ' + ( Math.ceil(parseFloat(sumValue * convertValue)) - Math.round(parseFloat(eurValue * convertValue)));
     }
         else {
-            document.getElementById('result2').innerText = 'Forint visszajáró: ' + (  Math.round(parseFloat(eurValue * convertValue )) - Math.round(parseFloat(sumValue * convertValue)) );
+            document.getElementById('result2').innerText = 'Ft vissza: ' + Math.floor((  Math.round(parseFloat(eurValue * convertValue )) - Math.ceil(parseFloat(sumValue * convertValue)) )/5)*5;
         }
     } else {
-        document.getElementById('result2').innerText = 'Helytelen eur összeg!';
+        document.getElementById('result2').innerText = 'Helytelen eur!';
     }
 });
 
@@ -40,15 +40,15 @@ document.getElementById('myftid').addEventListener('input', function() {
 
     if ( !isNaN(sumValue) && sumValue !== '' && ( eurValue % 5 === 0 ) && !isNaN(parseFloat(ftValue)) && ( ftValue >= 0 ) )  {
         if ( (sumValue * convertValue) > ( ( eurValue * convertValue ) + ftValue ) ) {
-            document.getElementById('result2').innerText = 'Forintban fizetendő: ' + ( Math.round(parseFloat(sumValue * convertValue)) - Math.round(parseFloat(eurValue * convertValue)) - Math.round(parseFloat(ftValue)) );
+            document.getElementById('result2').innerText = 'Ft fiz: ' + ( Math.ceil(parseFloat(sumValue * convertValue)) - Math.round(parseFloat(eurValue * convertValue)) - Math.round(parseFloat(ftValue)) );
         } else {
             var sumResult = sumValue * convertValue;
             var endResult = Math.round(parseFloat((eurValue * convertValue ) + parseFloat(ftValue) - sumResult));
 	    if ( isNaN(endResult) ) { 
         	document.getElementById('result2').innerText = 'Forint adat hiányzik vagy rossz, írjál 0-t, ha nincs!';
 	    } else {
-		endMessage = endResult < 0 ? "Forintban fizetendő még" : "Forint visszajáró";
-		endResult  = endResult < 0 ? endResult = endResult * -1 : endResult;
+		endMessage = endResult < 0 ? "Ft még" : "Ft vissza";
+		endResult  = endResult < 0 ? endResult = endResult * -1 : Math.floor(endResult/5)*5;
         	document.getElementById('result2').innerText = endMessage+': ' + endResult;
 	    }
         }
